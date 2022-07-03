e007c734750037d904cc7aa77aae7173bd055f35
Truxnit <truxnit@gmx.de>
2022-07-03 12:44:16 +0200

    update packages

--- src/utils/hooks/useForm.test.ts
+++ src/utils/hooks/useForm.test.ts
@@ -1,9 +1,9 @@
 import { renderHook } from "@testing-library/react-hooks";
 import { useForm } from "src/utils/hooks/useForm";
+
 describe("useForm", () => {
   it("initially", () => {
     const { result } = renderHook(() => useForm({ username: "" }));
-
-    /* expect(result.current.inputValue.username).toEqual("");*/
+    expect(result.current.inputValue.username).toEqual("");
   });
 });
